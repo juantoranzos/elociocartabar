372b3aa469be5e5d0733cb2bd868a37246758672
Juan Toranzos <toranzos628@gmail.com>
2025-09-03 17:39:25 -0300

    cambio de algunas fotos y agregue algunos tragos y cervezas tambien

--- db/db.js
+++ db/db.js
@@ -1,5 +1,10 @@
  import ginpuff from "@/public/ginpuff.webp";
-
+ import gintonic from "@/public/gintonic.jpeg";
+ import daikiri from "@/public/daikiri.jpeg";
+ import arcoiris from "@/public/arcoiris.jpeg";
+ import kaipiroshka from "@/public/kaipiroshka.jpeg";
+ import ginpicodulce from "@/public/ginpicodulce.jpeg";
+ import malibu from "@/public/malibu.jpeg";
 const tragos = [
   {
     id: 1,
@@ -22,15 +27,15 @@ const tragos = [
     titulo: "Gin Tonic",
     descripcion: "Podes elegir Limon o Frutos Rojos",
     precio: "$5000",
-    imagen: "https://img.freepik.com/foto-gratis/coctel-gin-tonic-beber-vaso-aislado-sobre-fondo-blanco_123827-21810.jpg?semt=ais_hybrid&w=740&q=80",
+    imagen: gintonic,
     descripcionLarga: "El Gin Tonic es un cóctel clásico que combina ginebra y agua tónica, conocido por su sabor refrescante y versatilidad. Se puede personalizar con diferentes guarniciones como rodajas de limón, pepino o frutos rojos, lo que lo convierte en una opción popular en bares y reuniones sociales. Su equilibrio entre la amargura de la tónica y los matices botánicos de la ginebra lo hacen ideal para disfrutar en cualquier ocasión."
   },
   {
     id: 4,
     titulo: "Daiquiri",
-    descripcion: "Cóctel tropical.",
+    descripcion: "Podes elegir de Maracuya o Frutilla",
     precio: "$5000",
-    imagen: "https://img.freepik.com/foto-gratis/coctel-martini-gota-limon-aislado-sobre-fondo-blanco_123827-24018.jpg?semt=ais_hybrid&w=740&q=80",
+    imagen: daikiri,
     descripcionLarga: "El Daiquiri es un cóctel tropical originario de Cuba, que se prepara con ron blanco, jugo de lima y azúcar. Es conocido por su sabor refrescante y equilibrado, y se puede servir en varias versiones, incluyendo el clásico, fresa o mango, siendo una opción popular en climas cálidos y en bares de playa."
   },
   {
@@ -62,7 +67,7 @@ const tragos = [
     titulo: "Kaipiroska",
     descripcion: "Cóctel brasileño.",
     precio: "$5000",
-    imagen: "https://thumbs.dreamstime.com/b/coctel-caipirinha-13310625.jpg",
+    imagen: kaipiroshka,
     descripcionLarga: "La Caipiroska es una variante del cóctel brasileño Caipirinha, que se prepara con vodka en lugar de cachaça. Este refrescante cóctel combina vodka, azúcar, jugo de lima y hielo triturado, y es conocido por su sabor equilibrado entre lo dulce y lo ácido. La Caipiroska es una opción popular en bares y fiestas, especialmente en climas cálidos."
   },
   {
@@ -78,7 +83,7 @@ const tragos = [
     titulo: "Arcoiris",
     descripcion: "Cóctel multicolor.",
     precio: "$5000",
-    imagen: "https://img.freepik.com/fotos-premium/sabroso-postre-gelatina-capas-vidrio-sobre-fondo-blanco_495423-55001.jpg?semt=ais_hybrid&w=740&q=80",
+    imagen: arcoiris,
     descripcionLarga: "El cóctel Arcoiris es una bebida visualmente impresionante que presenta capas de diferentes colores, creando un efecto similar al de un arcoíris. Se prepara utilizando varios licores y jugos con diferentes densidades, que se vierten cuidadosamente para mantener las capas separadas. Este cóctel es popular en fiestas y eventos especiales debido a su apariencia llamativa y su sabor dulce y frutal."
   },
   {
@@ -99,6 +104,22 @@ const tragos = [
   },
   {
     id: 13,
+    titulo: "Gin Pico Dulce",
+    descripcion: "Cóctel especial de la casa",
+    precio: "$5000",
+    imagen: ginpicodulce,
+    descripcionLarga: "El Gin Pico Dulce es un cóctel exclusivo de nuestra casa, que combina ginebra premium con un toque agua tónica y un toque de dulce, creando una experiencia única y refrescante. Este trago se sirve con hielo y se adorna con una rodaja de limón, ofreciendo un equilibrio perfecto entre los sabores botánicos de la ginebra y la dulzura del caramelo. Ideal para aquellos que buscan algo especial y diferente."
+  },
+  {
+    id: 14,
+    titulo: "Malibú",
+    descripcion: "Malibú de coco con jugo de Naranja",
+    precio: "$5000",
+    imagen: malibu,
+    descripcionLarga: "El Malibú es un cóctel tropical que combina el licor de coco Malibú con jugo de naranja, creando una bebida dulce y refrescante. Este trago es ideal para climas cálidos y se sirve con hielo, a menudo adornado con una rodaja de naranja o una cereza. El Malibú es perfecto para quienes buscan un sabor exótico y veraniego en su cóctel."
+  },
+  {
+    id: 15,
     titulo: "Heineken 1 L",
     descripcion: "Cerveza",
     precio: "$6000",
@@ -106,7 +127,7 @@ const tragos = [
     descripcionLarga: "Heineken es una cerveza lager de origen holandés, conocida por su sabor suave y refrescante. Se elabora con ingredientes de alta calidad, incluyendo lúpulo, cebada malteada y agua pura, lo que le confiere un perfil de sabor equilibrado y ligeramente amargo. Heineken es una de las cervezas más reconocidas a nivel mundial, apreciada por su consistencia y calidad en cada sorbo."
   },
   {
-    id: 14,
+    id: 16,
     titulo: "Imperial Golden | Lager 1 L",
     descripcion: "Cerveza",
     precio: "$5000",
@@ -114,7 +135,7 @@ const tragos = [
     descripcionLarga: "Imperial Golden es una cerveza lager de origen costarricense, conocida por su sabor ligero y refrescante. Elaborada con ingredientes de alta calidad, esta cerveza ofrece un perfil de sabor suave con notas de malta y lúpulo, lo que la convierte en una opción popular para disfrutar en climas cálidos y en reuniones sociales. Su color dorado y su textura ligera la hacen ideal para acompañar una variedad de comidas."
   },
   {
-    id: 15,
+    id: 17,
     titulo: "Quilmes 1 L",
     descripcion: "Cerveza",
     precio: "$4000",
@@ -122,7 +143,7 @@ const tragos = [
     descripcionLarga: "Quilmes es una cerveza argentina icónica, conocida por su sabor equilibrado y refrescante. Elaborada con ingredientes de alta calidad, Quilmes ofrece un perfil de sabor suave con un ligero amargor, lo que la convierte en una opción popular entre los amantes de la cerveza en Argentina y más allá. Su color dorado y su textura ligera la hacen ideal para disfrutar en cualquier ocasión, desde reuniones informales hasta celebraciones especiales."
   },
   {
-    id: 16,
+    id: 18,
     titulo: "Brhama 1 L",
     descripcion: "Cerveza",
     precio: "4000",
@@ -130,7 +151,7 @@ const tragos = [
     descripcionLarga: "Brahma es una cerveza brasileña reconocida por su sabor ligero y refrescante. Elaborada con ingredientes de alta calidad, Brahma ofrece un perfil de sabor suave con notas de malta y lúpulo, lo que la convierte en una opción popular para disfrutar en climas cálidos y en eventos sociales. Su color dorado y su textura ligera la hacen ideal para acompañar una variedad de comidas y celebraciones."
   },
   {
-    id: 17,
+    id: 19,
     titulo: "Miller 1 L",
     descripcion: "Cerveza",
     precio: "$5500",
@@ -138,7 +159,7 @@ const tragos = [
     descripcionLarga: "Miller es una marca de cerveza estadounidense conocida por su sabor ligero y refrescante. Elaborada con ingredientes de alta calidad, Miller ofrece un perfil de sabor suave con un ligero amargor, lo que la convierte en una opción popular entre los amantes de la cerveza. Su color dorado y su textura ligera la hacen ideal para disfrutar en cualquier ocasión, desde reuniones informales hasta celebraciones especiales."
   },
   {
-    id: 18,
+    id: 20,
     titulo: "1890 Quilmes 1 L",
     descripcion: "Cerveza",
     precio: "$4000",
@@ -146,12 +167,46 @@ const tragos = [
     descripcionLarga: "1890 Quilmes es una cerveza argentina clásica, conocida por su sabor auténtico y su rica historia. Elaborada con ingredientes de alta calidad, esta cerveza ofrece un perfil de sabor equilibrado con notas de malta y lúpulo, lo que la convierte en una opción popular entre los amantes de la cerveza en Argentina. Su color dorado y su textura ligera la hacen ideal para disfrutar en cualquier ocasión, desde reuniones informales hasta celebraciones especiales."
   },
   {
-    id: 19,
+    id: 21,
     titulo: "Salta Negra 1 L",
     descripcion: "Cerveza",
     precio: "$4500",
     imagen: "https://jumboargentina.vtexassets.com/arquivos/ids/180511/Cerveza-Salta-Negra-Cerveza-Salta-Negra-970-Ml-1-12496.jpg?v=636383396831030000",
     descripcionLarga: "Salta Negra es una cerveza argentina de estilo stout, conocida por su sabor intenso y robusto. Elaborada con maltas oscuras, esta cerveza ofrece un perfil de sabor rico con notas de chocolate, café y caramelo. Su color oscuro y su cuerpo completo la hacen ideal para aquellos que disfrutan de cervezas con carácter, siendo una opción popular en el norte de Argentina y entre los amantes de las cervezas artesanales."
-  }
+  },
+  {
+    id: 22,
+    titulo: "Patagonia IPA 710 ml",
+    descripcion: "Cerveza",
+    precio: "$6000",
+    imagen: "https://menorcoste.com.ar/media/catalog/product/cache/733e84b26907731b1236b698d5769bc3/0/0/0000000019076_19076_0_2024-07-27_11_35_18.jpg",
+    descripcionLarga: "Patagonia IPA es una cerveza artesanal argentina de estilo India Pale Ale, conocida por su sabor intenso y lupulado. Esta cerveza ofrece un perfil de sabor complejo con notas cítricas y florales, equilibradas por un amargor característico del estilo IPA. Patagonia IPA es una opción popular entre los amantes de las cervezas artesanales que buscan una experiencia de sabor audaz y distintiva."
+  },
+   {
+    id: 23,
+    titulo: "Patagonia Amber Lager 710 ml",
+    descripcion: "Cerveza",
+    precio: "$6000",
+    imagen: "https://menorcoste.com.ar/media/catalog/product/cache/733e84b26907731b1236b698d5769bc3/0/0/0000000019075_19075_0_2024-04-10_17_35_05.jpg",
+    descripcionLarga: "Patagonia Amber Lager es una cerveza artesanal argentina de estilo Amber Lager, conocida por su sabor equilibrado y maltoso. Esta cerveza ofrece un perfil de sabor rico con notas de caramelo y toffee, complementadas por un amargor suave que la hace fácil de beber. Patagonia Amber Lager es una opción popular entre los amantes de las cervezas artesanales que buscan una experiencia de sabor completa y satisfactoria."
+  },
+     {
+    id: 24,
+    titulo: "Corona 710 ml",
+    descripcion: "Cerveza",
+    precio: "$6000",
+    imagen: "https://menorcoste.com.ar/media/catalog/product/cache/733e84b26907731b1236b698d5769bc3/0/0/0050000000211_50000000211_0_2022-01-31_14_25_05.jpg",
+    descripcionLarga: "Corona es una cerveza lager mexicana conocida por su sabor ligero y refrescante. Elaborada con ingredientes de alta calidad, Corona ofrece un perfil de sabor suave con un toque cítrico, lo que la convierte en una opción popular para disfrutar en climas cálidos y en eventos sociales. Su color dorado y su textura ligera la hacen ideal para acompañar una variedad de comidas, especialmente mariscos y platos frescos."
+  },
+       {
+    id: 25,
+    titulo: "Stella Artois 1 L",
+    descripcion: "Cerveza",
+    precio: "$6000",
+    imagen: "https://static.wixstatic.com/media/d2b1c5_62629d96d63a4ceaaf92481bb25ef628~mv2.jpg/v1/fit/w_500,h_500,q_90/file.jpg",
+    descripcionLarga: "Stella Artois es una cerveza lager belga reconocida por su sabor equilibrado y refinado. Elaborada con ingredientes de alta calidad, Stella Artois ofrece un perfil de sabor suave con notas de malta y lúpulo, lo que la convierte en una opción popular entre los amantes de la cerveza en todo el mundo. Su color dorado y su textura ligera la hacen ideal para disfrutar en cualquier ocasión, desde reuniones informales hasta celebraciones especiales."
+  },
+  
 ];
+
 export default tragos;
