d5c9114e0e010ee3098dd64648bd3fc4ed538dcb
Juan Toranzos <toranzos628@gmail.com>
2025-09-05 19:38:41 -0300

    componente promo actualizado

--- components/promo.js
+++ components/promo.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 const PromoComponent = () => {
   return (
-    <div>
-        <p className='text-3xl text-center p-4 m-2 text-amber-600'>2x1 Viernes y Sabado de 23:00hs a 00:00hs</p>
+    <div className='bg-black text-white flex flex-col justify-center items-center m-4 border-4 border-amber-600 rounded-lg shadow-lg shadow-amber-600/50'>
+        <p className='text-3xl text-center p-4 m-2 text-amber-600 '>Viernes y Sabado <br/> 2x1 en Tragos <br/> De 23hs a 00hs</p>
     </div>
   )
 }
 
-export default PromoComponent
+export default PromoComponent 
